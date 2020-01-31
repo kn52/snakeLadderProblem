2742b7771ebcc40ac8db69001e9c994c981286d0
kn52 <ashish52922@gmail.com>
2020-01-31 18:44:32 +0530

    ADDED:switch case to check player options noplay snake ladder

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -5,8 +5,34 @@ currentPosition=$START
 echo "Welcome to Snake And Ladder Game"
 rollDice()
 {
-	value=$((RANDOM%6))
+	value=$(( 1 + $((RANDOM%6)) ))
 	echo "Dice Output: $value"
 }
+playerOptions()
+{
+	local option=$(( $((RANDOM%3)) + 1 ))
+	echo "Option: $option"
+	rollDice
+	case $option in
+		1)
+			currentPosition=$currentPosition
+			;;
+		2)
+			currentPosition=$(( $currentPosition + $value ))
+			;;
+		3)	
+			currentPosition=$(( $currentPosition - $value))
+			if (( $currentPosition < 0 ))
+			then
+				currentPosition=$START
+			fi
+			;;
+		*)
+			echo "Option: $(( $((RANDOM%3)) + 1 ))"
+			echo "Option not available"
+			;;
+	esac
+	echo "Current Position of player: $currentPosition"
+}
 
-rollDice
+playerOptions
